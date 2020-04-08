e00d862266c7d7c39948bc7e5f106a2f50a9b051
guansheng <guansheng@xiaohongshu.com>
2020-04-09 00:14:41 +0800

    初始化项目版本

--- src/main/java/com/example/demo/HelloWord.java
+++ src/main/java/com/example/demo/HelloWord.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
